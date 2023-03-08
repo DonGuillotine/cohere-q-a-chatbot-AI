773c499377970964112c58b2db6d7f72e864e3cd
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-08 23:06:41 +0100

    Added a method to generate text

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import cohere
 from secrets import secrets;
 
-
+# Providing API Key Credentials
 class CoHere:
     def __init__(self, api_key):
         self.co = cohere.Client('API_KEY', '2021-11-08')
 
+# A method to generate a text
     def cohere(self, question):
         return self.co.generate(
-            model=&#39;medium&#39;,
-        )
+            model='medium',
+            prompt=stevenQa(question),
+            max_tokens=50,
+            temperature=1
+        ).generations[0].text
