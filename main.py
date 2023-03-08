5dafb2284f0e9e1c501b750fc5932059337f1ea9
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-08 22:59:05 +0100

    API Key Credentials set I

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ from secrets import secrets;
 class CoHere:
     def __init__(self, api_key):
         self.co = cohere.Client('API_KEY', '2021-11-08')
+
+    def cohere(self, question):
+        return self.co.generate(
+            model=&#39;medium&#39;,
+        )
