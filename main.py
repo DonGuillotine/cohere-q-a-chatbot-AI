8a084898461422291305b02b163e13ca0132bfd6
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-08 23:09:49 +0100

    Writing a prompt for my model

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ class CoHere:
     def __init__(self, api_key):
         self.co = cohere.Client('API_KEY', '2021-11-08')
 
+
 # A method to generate a text
     def cohere(self, question):
         return self.co.generate(
             model='medium',
-            prompt=stevenQa(question),
+            prompt=myPrompt(question),
             max_tokens=50,
             temperature=1
         ).generations[0].text
+    
+
+# Writing a prompt for my model
+    def myPrompt(question):
+        return 'Donald is a chatbot that answers questions:'
